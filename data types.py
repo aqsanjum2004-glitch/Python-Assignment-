3419213b7d2e2b3c490ea05bd826d21e709c0f41
aqsanjum2004-glitch <aqsanjum2004@gmail.com>
2026-02-21 00:20:25 +0530

    Add data types example in data types.py

---
+++ data types.py
@@ -0,0 +1,16 @@
+# String
+name = "Aqsa"
+
+# Integer
+age = 21
+
+# Float
+percentage = 85.5
+
+# Boolean
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Percentage:", percentage)
+print("Is Student:", is_student)

2dd0571f515f5026e44dd4801fe2ecc74a34d7f6
aqsanjum2004-glitch <aqsanjum2004@gmail.com>
2026-02-21 00:57:08 +0530

    Add nested if structure for age verification
    Implement nested if statement for age and ID check

---
+++ Nested if.py
@@ -0,0 +1,15 @@
+age = 20
+has_id = True
+
+if age >= 18:
+    if has_id:
+        print("You can enter")
+    else:
+        print("ID is required")
+else:
+    print("You are underage")
+  .............................
+#syntax for Nested if 
+if condition:
+    if another_condition:
+        print("Something")

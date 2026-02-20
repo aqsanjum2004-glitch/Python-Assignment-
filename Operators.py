e8215634ff62ee68035a003c2514653ff9a98d5b
aqsanjum2004-glitch <aqsanjum2004@gmail.com>
2026-02-21 00:44:36 +0530

    Add comparison, logical, and assignment operators examples

---
+++ Operators.py
@@ -0,0 +1,52 @@
+#Comparison Operators 
+a = 10
+b = 5
+
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater Than:", a > b)
+print("Less Than:", a < b)
+print("Greater or Equal:", a >= b)
+print("Less or Equal:", a <= b)
+
+# Output:
+# Equal: False
+# Not Equal: True
+# Greater Than: True
+# Less Than: False
+# Greater or Equal: True
+# Less or Equal: False
+...........................
+#Logical Operators 
+x = True
+y = False
+
+print("AND:", x and y)
+print("OR:", x or y)
+print("NOT x:", not x)
+
+# Output:
+# AND: False
+# OR: True
+# NOT x: False
+.............................
+#Assignment Operators 
+num = 10
+
+num += 5
+print("After += :", num)
+
+num -= 3
+print("After -= :", num)
+
+num *= 2
+print("After *= :", num)
+
+num /= 4
+print("After /= :", num)
+
+# Output:
+# After += : 15
+# After -= : 12
+# After *= : 24
+# After /= : 6.0

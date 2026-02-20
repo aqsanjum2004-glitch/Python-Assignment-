f7a243ab70d463393d29ad0a57b5f93b3addcf00
aqsanjum2004-glitch <aqsanjum2004@gmail.com>
2026-02-21 01:06:10 +0530

    Implement define and add functions in Functions.py
    Added functions for greeting and addition.

---
+++ Functions.py
@@ -0,0 +1,20 @@
+def greet():
+    print("Hello, welcome to Python!")
+
+greet()
+
+#Function with Parameters
+def greet(name):
+    print("Hello", name)
+
+greet("Aqsa")
+
+#Function with Return Value
+def add(a, b):
+    return a + b
+
+result = add(10, 5)
+print("Sum is:", result)
+
+# Output:
+# Sum is: 15

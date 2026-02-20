366351223b9b67106c911e47764bcd05630b3dd2
aqsanjum2004-glitch <aqsanjum2004@gmail.com>
2026-02-21 00:54:32 +0530

    Add conditional statements examples in Python

---
+++ Conditional statements.py
@@ -0,0 +1,31 @@
+#Simple if Statement
+age = 20
+
+if age >= 18:
+    print("You are eligible to vote")
+  ..............................................................
+#  if-else Statement
+age = 16
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You are not eligible to vote")
+
+# Output:
+# You are not eligible to vote
+....................................................................
+#if-elif-else (Multiple Conditions)
+  marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+# Output:
+# Grade B

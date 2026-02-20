03104f98bd23fe5defb96e0d4becbd4b9cf564ad
aqsanjum2004-glitch <aqsanjum2004@gmail.com>
2026-02-21 01:02:49 +0530

    Create Looping statements example in Python
    Add examples of for and while loops in Python.

---
+++ Looping statments.py
@@ -0,0 +1,34 @@
+#for loop
+for i in range(5):
+    print("Hello")
+#Output
+#Hello
+#Hello
+#Hello
+#Hello
+#Hello
+
+#Print Numbers
+for i in range(1, 6):
+    print(i)
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+#While Loop
+num = 1
+
+while num <= 5:
+    print(num)
+    num += 1
+  #Output
+
+#1
+#2
+#3
+#4
+#5
